444afb3fd77d4bef95d1a8d6ca24e86294a901c9
sant-El <eliasvsr1@gmail.com>
2024-07-24 19:25:38 -0300

    Listar

--- jogos.js
+++ jogos.js
@@ -9,21 +9,44 @@ const criar = () => {
   const preco = prompt(`Preço : `);
   const estudio = prompt(`Estúdio: `);
   const sequencia = prompt(`Qual é a sequência do jogo? `);
-  if (nome != "" &&
-   anoLancamento >= 1962 &&
+  if (
+    nome != "" &&
+    anoLancamento >= 1962 &&
     anoLancamento <= 2024 &&
-     duracao > 0 &&
-      preco == 0 &&
-       estudio != "" &&
-        sequencia > 0 &&
-         (sequencia < jogos.length ||
-             jogos.length == 0) 
-          ) {
-            jogos.push({
-                nome, anoLancamento, duracao, preco, estudio, sequencia
-            })
-        console.log (`Jogo cadastrado com sucesso! `)
-  }else {
-    console.log (`Ops! Algo deu errado. :()`)
+    duracao > 0 &&
+    preco == 0 &&
+    estudio != "" &&
+    sequencia > 0 &&
+    (sequencia < jogos.length || jogos.length == 0)
+  ) {
+    jogos.push({
+      nome,
+      anoLancamento,
+      duracao,
+      preco,
+      estudio,
+      sequencia,
+    });
+    console.log(`Jogo cadastrado com sucesso! `);
+  } else {
+    console.log(`Ops! Algo deu errado. :()`);
+  }
+};
+
+const listar = () => {
+  if (jogos.length == 0) {
+    console.log(`Nenhum jogo encontrado!`);
+  } else {
+    jogos.forEach((jogo, index) => {
+      console.log(`
+        ${index + 1}.
+        Nome: ${jogo.nome}
+        Ano de Lançamento: ${jogo.anoLancamento}
+        Duração: ${jogo.duracao}
+        Preço: ${jogo.preco}
+        Estúdio: ${jogo.estudio}
+        Sequência; ${jogo.sequencia}
+        `);
+    });
   }
 };
